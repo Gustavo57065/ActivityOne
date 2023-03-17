2206980091f4ae0b3087eac733da7a8bb268b193
Gustavo Silva Quieregato <quieregatog@alunos.utfpr.edu.br>
2023-03-17 17:55:18 -0300

    First commit in github

--- app/src/main/java/com/example/firstactivity/MainActivity.kt
+++ app/src/main/java/com/example/firstactivity/MainActivity.kt
@@ -1,15 +1,22 @@
 package com.example.firstactivity
 
+import android.annotation.SuppressLint
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
-    var btnClickMe = findViewById<Button>(R.id.button)
-        var text = findViewById<TextView>(R.id.text_id)
+        val btnClickMe = findViewById<Button>(R.id.button)
+        val text = findViewById<TextView>(R.id.textView)
+        var timesClided = 0;
+        btnClickMe.setOnClickListener{
+            timesClided++
+            text.text = timesClided.toString()
+        }
     }
 }
